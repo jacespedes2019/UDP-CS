c0dad9f3fd2a41dc1c97cf0024475eeacc516559
Jairo Céspedes <ja.cespedes@uniandes.edu.co>
2022-04-08 15:08:07 -0500

    a

--- src/ClienteUDP.java
+++ src/ClienteUDP.java
@@ -156,10 +156,10 @@ public class ClienteUDP extends Thread{
 		}
 		else {
 			if(arch.contains("100MB.txt")){
-				totalArch=100000000;
+				totalArch=104800000;
 			}
 			else {
-				totalArch=240000000;
+				totalArch=262100000;
 			}
 		}
 		return totalArch;
